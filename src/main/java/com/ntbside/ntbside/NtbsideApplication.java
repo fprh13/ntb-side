0d18e8c865c8f9dbf295ba557ad79694a60318a5
fprh13 <fprh13@gamil.com>
2023-08-16 18:19:21 +0900

    [ADD] first commit

---
+++ src/main/java/com/ntbside/ntbside/NtbsideApplication.java
@@ -0,0 +1,13 @@
+package com.ntbside.ntbside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtbsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtbsideApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ntbside/ntbside/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ntbside.ntbside.repository;
+
+import com.ntbside.ntbside.DTO.UserDTO;
+import com.ntbside.ntbside.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    List<UserEntity> findAllByOrderByScoreDesc();
+
+
+}
